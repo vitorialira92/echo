9d356a621b8040ac655d56b117dad1ea3643656d
Vitória Tenório <vitoriabusinesscontact@gmail.com>
2026-06-14 23:58:52 -0300

    Security

---
+++ src/main/java/liraz/echo/security/AppUserDetailsService.java
@@ -0,0 +1,34 @@
+package liraz.echo.security;
+
+import liraz.echo.domain.user.User;
+import liraz.echo.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())))
+                .build();
+    }
+}
